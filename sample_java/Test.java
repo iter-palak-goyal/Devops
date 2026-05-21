96f4e30192be01e1ab9260c43f30644f810b63eb
iter-palak-goyal <iter.palak.goyal@gmail.com>
2026-05-21 15:22:04 +0530

    fixed sample_java folder

---
+++ sample_java/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+  static void add(){
+
+    int a=48;
+    int b=65;
+    int result = a*b;
+    System.out.println("The sum is " +result);
+    
+  }
+  public static void main(String[] args)
+  {
+    add();
+  }
+}
